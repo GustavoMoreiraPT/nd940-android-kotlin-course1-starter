74213c191d61dfa1e55f023d0ad6860a4cd1c728
Gustavo <gustavo.moreira@ctw.bmwgroup.com>
2020-12-31 19:15:24 +0000

    Intermideiate implementation

--- starter/app/src/main/java/com/udacity/shoestore/screens/listing/ListingFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/listing/ListingFragment.kt
@@ -4,15 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ListingFragmentBinding
+import androidx.lifecycle.Observer
+import com.udacity.shoestore.databinding.DetailFragmentBinding
 
 class ListingFragment: Fragment() {
+
+    private lateinit var viewModel: ListingViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        val binding: ListingFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.listing_fragment, container, false)
+
+        binding.addShoe.setOnClickListener{
+            findNavController().navigate(ListingFragmentDirections.actionListingFragmentToDetailFragment())
+        }
+
+        viewModel.shoes.observe(
+            viewLifecycleOwner,
+            { shoes ->
+                shoes.forEach { shoe ->
+                    val shoesBinding = DataBindingUtil.inflate<DetailFragmentBinding>(
+                        inflater,
+                        R.layout.detail_fragment,
+                        binding.titleLayout,
+                        false
+                    )
+                    shoesBinding.nameText.text = shoe.name
+                    shoesBinding.companyText.text = shoe.company
+                    shoesBinding.sizeText.text = shoe.size.toInt().toString()
+                    binding.titleLayout.addView(shoesBinding.root)
+                }
+            }
+        )
+        setHasOptionsMenu(true)
+        return binding.root
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/screens/listing/ListingViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/listing/ListingViewModel.kt
@@ -1,6 +1,33 @@
 package com.udacity.shoestore.screens.listing
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
 
 class ListingViewModel : ViewModel() {
+
+    private val _shoes = MutableLiveData<List<Shoe>>()
+    val shoes: LiveData<List<Shoe>>
+        get() = _shoes
+
+    init {
+        _shoes.value = seedShoes()
+    }
+
+    fun add(shoe: Shoe){
+        val newInventory = mutableListOf<Shoe>()
+        newInventory.addAll(_shoes.value!!.asIterable())
+        newInventory.add(shoe)
+        _shoes.value = newInventory
+    }
+
+    private fun seedShoes(): List<Shoe> {
+        val shoes = mutableListOf<Shoe>()
+        for (i in 1..10){
+            shoes.add(Shoe("Shoe$i", 37.0 + i, "Company$i", "This shoe is called Shoe$i" ))
+        }
+
+        return shoes
+    }
 }
